25f2bc54b8cce1c43c1eab5110e9c8a921143200
annaK31 <148232742+annaK31@users.noreply.github.com>
2024-03-15 18:26:13 +0200

    updated task 2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,37 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+const delayForm = document.querySelector('.form');
+
+delayForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const timer = event.currentTarget.elements.delay.value;
+  const radio = event.currentTarget.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(timer, radio);
+      if (radio === 'fulfilled') {
+        resolve();
+      } else {
+        reject();
+      }
+    }, timer);
+  });
+
+  // Registering promise callbacks
+  promise
+    .then(value => {
+      iziToast.success({
+        color: 'green',
+        position: 'topRight',
+        message: `✅ Fulfilled promise in ${timer}ms`,
+      });
+    })
+    .catch(error => {
+      iziToast.error({
+        color: 'red',
+        position: 'topRight',
+        message: `❌ Rejected promise in ${timer}ms`,
+      });
+    });
+});
